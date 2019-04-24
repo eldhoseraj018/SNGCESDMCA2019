fc622bf1a9449746be3a1b88261df7f113124df7
eldhoseraj018 <48010581+eldhoseraj018@users.noreply.github.com>
2019-04-24 15:49:32 +0530

    commi2

--- avg.sh
+++ avg.sh
@@ -3,3 +3,11 @@ read a b c d e
 sum=`expr $a + $b + $c + $d + $e`
 avg=`expr $sum / 5`
 echo $avg
+fact=1
+
+while [ $avg -gt 1 ]
+do
+  fact=$((fact * avg))  
+  avg=$((avg - 1))   
+done
+echo $fact
